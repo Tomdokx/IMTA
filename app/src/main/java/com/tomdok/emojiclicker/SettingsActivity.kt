30bb3208387fdf07e9ce332293800661d479c396
Tomdokx <tomdokx@gmail.com>
2021-06-12 19:50:48 +0200

    add option to rename + turn off animations (need to finish after adding to Store.

--- app/src/main/java/com/tomdok/emojiclicker/SettingsActivity.kt
+++ app/src/main/java/com/tomdok/emojiclicker/SettingsActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.system.Os.rename
 import android.widget.*
+import database.GameDatabase
+import database.Settings
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import store.Store
 
 class SettingsActivity : AppCompatActivity() {
@@ -41,6 +46,29 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         buttonBack.setOnClickListener { goBackToMenu() }
+
+        buttonRename.setOnClickListener { rename() }
+
+        switchAnimation?.setOnCheckedChangeListener { _, isChecked->
+
+            runBlocking {
+
+                coroutineScope {
+
+                    launch {
+
+                        val settingsUpdate = Settings(null,isChecked,store.player.name)
+                        GameDatabase.getInstance(applicationContext).settingsDAO.update(settingsUpdate)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun rename() {
+
+        val newName = editTextRename.text.toString()!!
+        store.player.name = newName
     }
 
     private fun goBackToMenu() {
